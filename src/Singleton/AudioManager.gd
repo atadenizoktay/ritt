c73d3c353710e58cdf8dc3d362ad79a66540fffd
Ata Deniz Oktay <oktayatadeniz@gmail.com>
2022-05-28 02:37:03 +0300

    added simple beat counter

---
+++ src/Singleton/AudioManager.gd
@@ -0,0 +1,30 @@
+extends Node
+
+
+"""
+	Node: AudioManager
+	Type: Singleton
+	Description: This script controls the audio of the game.
+"""
+
+
+signal beat_dropped
+
+onready var _BeatPlayer: AudioStreamPlayer = $BeatPlayer
+onready var _BeatCounter: MidiPlayer = $BeatCounter
+
+
+func _ready() -> void:
+	_initialize_beat_related_nodes()
+
+
+func _initialize_beat_related_nodes() -> void:
+	_BeatPlayer.play()
+	_BeatCounter.play()
+	
+	
+func _on_BeatCounter_midi_event(\
+		channel: MidiPlayer.GodotMIDIPlayerChannelStatus, \
+		event: SMF.MIDIEvent) -> void:
+	if event.type == SMF.MIDIEventType.note_on:
+		emit_signal("beat_dropped")
